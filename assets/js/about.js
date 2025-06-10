c2a463161f5b5a9ecaedd5c9fa2317dde62d6c99
hgkim <hgkim@rianag.com>
2025-06-10 13:56:10 +0900

    pc버전 완료

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,27 +1,201 @@
-const paths = document.querySelectorAll('path');
+ gsap.registerPlugin(ScrollTrigger)
 
-paths.forEach((path, i) => {
-const length = path.getTotalLength();
-path.style.stroke = "white";
-path.style.strokeWidth = "2";
-path.style.fill = "none";
+$(document).ready(function () {
+    /* 스와이퍼 */
+    var option_sec03 = {
+        slidesPerView: 4.5,
+        spaceBetween:10,
+        keyboard: {
+            enabled: true,
+        },
+    };
+    var swiper = new Swiper('.about_swiper .visual.swiper-container', option_sec03);
 
-gsap.set(path, {
-    strokeDasharray: length,
-    strokeDashoffset: length
-});
+    /* gsap path 이벤트 */
+    const paths = document.querySelectorAll('path');
 
-gsap.to(path, {
-    strokeDashoffset: 0,
-    duration: 2,
-    delay: i * 0.3,
-    ease: "power2.out",
-    onComplete: () => {
-    gsap.to(path, {
-        fill: "white",
-        duration: 0.5,
-        ease: "power1.inOut"
+    paths.forEach((path, i) => {
+        const length = path.getTotalLength();
+        path.style.stroke = "white";
+        path.style.strokeWidth = "2";
+        path.style.fill = "none";
+
+        gsap.set(path, {
+            strokeDasharray: length,
+            strokeDashoffset: length
+        });
+
+        gsap.to(path, {
+            strokeDashoffset: 0,
+            duration: 2,
+            delay: i * 0.3,
+            ease: "power2.out",
+            onComplete: () => {
+            gsap.to(path, {
+                fill: "white",
+                duration: 0.5,
+                ease: "power1.inOut"
+            });
+            }
+        });
     });
+
+    /* gsap 이벤트 */
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'.about_vision h2',
+            start:'top 90%',
+            end:'50% 100%',
+            toggleActions: 'play none none none',
+            // markers:true,
+        }
+    })
+    .to('.about_vision h2 p:nth-child(1)',{y:'0px', duration:1, ease:'none', opacity:1},0.2)
+    .to('.about_vision h2 p:nth-child(2)',{y:'0px', duration:1, ease:'none', opacity:1},0.4)
+    .to('.about_vision h2 p:nth-child(3)',{y:'0px', duration:1, ease:'none', opacity:1},0.6)
+
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'.about_vision .one',
+            start:'top 90%',
+            end:'50% 100%',
+            toggleActions: 'play none none none',
+            // markers:true,
+        }
+    })
+    .to('.about_vision .one dd',{x:'0px', duration:1, ease:'none', opacity:1},0.2)
+
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'.about_vision .two',
+            start:'top 90%',
+            end:'90% 100%',
+            toggleActions: 'play none none none',
+            // markers:true,
+        }
+    })
+    .to('.about_vision .two dd dl:nth-child(1)',{duration:1, ease:'none', opacity:1},0.2)
+    .to('.about_vision .two dd dl:nth-child(2)',{duration:1, ease:'none', opacity:1},0.4)
+    .to('.about_vision .two dd dl:nth-child(3)',{duration:1, ease:'none', opacity:1},1)
+    .to('.about_vision .two dd dl:nth-child(4)',{duration:1, ease:'none', opacity:1},1.2)
+
+
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:'.wrap > img',
+            start:'top 90%',
+            end:'50% 100%',
+            toggleActions: 'play none none none',
+            // markers:true,
+        }
+    })
+    .to('.wrap > img',{y:'0px', duration:1, ease:'none', opacity:1},0.2)
+
+/* count evetn */
+  const $targets = $('.about_swiper .swiper-slide strong');
+  const values = [8, 2, 1, 1, 1, 3]; // 각 strong 요소의 목표값
+
+  $targets.each(function (index, element) {
+    const $el = $(element);
+    const endValue = values[index];
+
+    // 초기값
+    $el.text('0');
+
+    ScrollTrigger.create({
+      trigger: '.about_awards',
+      start: '50% 80%',
+      once: true,
+      onEnter: () => {
+        gsap.to({ val: 0 }, {
+          val: endValue,
+          duration: 0.5 + endValue * 0.2, // 값이 클수록 duration을 늘림
+          ease: 'power1.out',
+          snap: 'val', // 자연스러운 정수 변화
+          onUpdate: function () {
+            $el.text(this.targets()[0].val);
+          }
+        });
+      }
+    });
+  });
+
+  const $cursor = $('.about_swiper .custom-cursor');
+  const $area = $('.about_swiper');
+  let isHovering = false;
+
+  $(document).on('mousemove', function (e) {
+    if (isHovering) {
+      $cursor.css({
+        left: e.clientX + 'px',
+        top: e.clientY + 'px'
+      });
     }
+  });
+
+  $area.on('mouseenter', function () {
+    isHovering = true;
+    $cursor.css('opacity', 1);
+    $(this).addClass('hide-cursor');
+  });
+
+  $area.on('mouseleave', function () {
+    isHovering = false;
+    $cursor.css('opacity', 0);
+    $(this).removeClass('hide-cursor');
+  });
+
+    let triggered = false;
+
+    ScrollTrigger.create({
+        trigger: ".about_partners",
+        start: "top 80%",
+        once: true, // 스크롤 한 번만 작동
+        onEnter: function () {
+            if (triggered) return;
+            triggered = true;
+
+            $(".about_partners ul li").each(function (index) {
+                gsap.fromTo($(this), 
+                    { opacity: 0, y: 50 }, 
+                    {
+                        opacity: 1,
+                        y: 0,
+                        duration: 0.6,
+                        ease: "bounce.out",
+                        delay: index * 0.1
+                    }
+                );
+            });
+        }
+    });
+
+    const $scroll = $('.about_visual .custom-cursor');
+    const $target = $('.about_visual');
+    let isHover = false;
+
+  $(document).on('mousemove', function (e) {
+    if (isHover) {
+      $scroll.css({
+        left: e.clientX + 'px',
+        top: e.clientY + 'px'
+      });
+    }
+  });
+
+  $target.on('mouseenter', function () {
+    isHover = true;
+    $scroll.css('opacity', 1);
+    $(this).addClass('hide-cursor');
+  });
+
+  $target.on('mouseleave', function () {
+    isHover = false;
+    $scroll.css('opacity', 0);
+    $(this).removeClass('hide-cursor');
+  });
+
+
+
 });
-});
+
