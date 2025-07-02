34bfb5de67a712ea34c560664c675293f06bcc2a
hgkim <hgkim@rianag.com>
2025-07-02 16:06:55 +0900

    about 영역 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -98,9 +98,9 @@ $(document).ready(function () {
                 if (isType2 && isWhite) {
                     $logo.attr('src', originalLogoSrc);
                 } else if (isType2) {
-                    $logo.attr('src', '/assets/images/common/h1_logo_black.png');
+                    $logo.attr('src', toBlackLogo(originalLogoSrc));
                 } else {
-                    $logo.attr('src', originalLogoSrc.replace('_black.png', '.png'));
+                    $logo.attr('src', toWhiteLogo(originalLogoSrc));
                 }
 
                 if (isType3) {
@@ -122,9 +122,9 @@ $(document).ready(function () {
                 $btnHam.parent().addClass('close');
 
                 if (isType2) {
-                    $logo.attr('src', '/assets/images/common/h1_logo_black.png');
+                    $logo.attr('src', toBlackLogo(originalLogoSrc)); // ✅
                 } else {
-                    $logo.attr('src', originalLogoSrc.split('.')[0] + '_black.png');
+                    $logo.attr('src', toBlackLogo(originalLogoSrc));
                 }
 
                 if (isType3) {
@@ -198,49 +198,14 @@ function lazyLoads() {
     }
 }
 
-// function initHeaderScrollToggle() {
-//   let lastScrollTop = 0;
-//   const delta = 5;
-//   const $Header = $('header');
-
-//   $(window).on('scroll.headerToggle', function () {
-//     if ($('.site_map.active').length > 0) return;
-
-//     const st = $(this).scrollTop();
-//     if (Math.abs(lastScrollTop - st) <= delta) return;
-
-//     const isMobile = window.innerWidth <= 767;
-
-//     if (st > lastScrollTop && st > 100) {
-//       $Header.addClass('hide');
-//       if (isMobile) {
-//         $Header.removeClass('on');
-//       }
-//     } else {
-//       $Header.removeClass('hide');
-//       if (isMobile) {
-//         $Header.addClass('on');
-
-//         // ✅ 스크롤 맨 위이면 on 제거
-//         if (st <= 5) {
-//           $Header.removeClass('on');
-//         }
-//       }
-//     }
-
-//     lastScrollTop = st;
-//     console.log('scrollTop:', st);
-//   });
-// }
-
 function initHeaderScrollToggle() {
   let lastScrollTop = 0;
   const delta = 5;
   const $Headers = $('header');
   const $Logo = $Headers.find('h1 img');
   const originalSrc = $Logo.attr('src');
-  const whitekSrc = originalSrc.replace('_black.png', '.png');  // 원래 로고 복귀용
-  const blacSrc = originalSrc.replace('.png', '_black.png');    // black 로고 버전
+  const whitekSrc = toWhiteLogo(originalSrc);  // 원래 로고 복귀용
+  const blacSrc = toBlackLogo(originalSrc);;    // black 로고 버전
 
   $(window).on('scroll.headerToggle', function () {
     if ($('.site_map.active').length > 0) return;
@@ -438,18 +403,10 @@ function toast_close(_toast) {
     }, 200);
 }
 
+function toBlackLogo(src) {
+  return src.includes('_black.png') ? src : src.replace('.png', '_black.png');
+}
 
-// function animateScaleTarget($area, scaleFrom = 1.2, scaleTo = 1, duration = 1.5, delay = 0, withFade = false) {
-//     if (!$area || $area.length === 0) return;
-
-//     gsap.set($area, { scale: scaleFrom, opacity: withFade ? 0 : 1 });
-
-//     gsap.to($area, {
-//         scale: scaleTo,
-//         opacity: withFade ? 1 : undefined,
-//         duration: duration,
-//         delay: delay,
-//         ease: "power3.out"
-//     });
-//     console.log('big')
-// }
+function toWhiteLogo(src) {
+  return src.replace('_black.png', '.png');
+}
